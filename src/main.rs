4b7db328e8ecf169e6300962dc9685cc1ac1815b
Will Train <87541128+wt-314159@users.noreply.github.com>
2025-11-15 17:33:56 +0000

    Loading file contents at run time.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use iced::widget::{container, text_editor};
-use iced::{Element, Sandbox, Settings, Theme};
+use std::path::Path;
+use std::sync::Arc;
+
+use iced::widget::{column, container, horizontal_space, row, text, text_editor};
+use iced::{Application, Command, Element, Length, Settings, Theme, executor};
+use tokio::io;
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
@@ -12,35 +16,70 @@ struct Editor {
 #[derive(Debug, Clone)]
 enum Message {
     Edit(text_editor::Action),
+    FileOpened(Result<Arc<String>, io::ErrorKind>),
 }
 
-impl Sandbox for Editor {
+impl Application for Editor {
+    type Executor = executor::Default;
     type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            content: text_editor::Content::with(include_str!("main.rs")),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                content: text_editor::Content::new(),
+            },
+            Command::perform(
+                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
+                Message::FileOpened,
+            ),
+        )
     }
 
     fn title(&self) -> String {
         "A cool editor".to_string()
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::Edit(action) => {
                 self.content.edit(action);
             }
-        }
+            Message::FileOpened(result) => {
+                if let Ok(content) = result {
+                    self.content = text_editor::Content::with(&content);
+                }
+            }
+        };
+
+        Command::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
         let input = text_editor(&self.content).on_edit(Message::Edit);
-        container(input).padding(10).into()
+
+        let position = {
+            let (line, column) = self.content.cursor_position();
+
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+
+        let status_bar = row![horizontal_space(Length::Fill), position];
+
+        container(column![input, status_bar].spacing(10))
+            .padding(10)
+            .into()
     }
 
     fn theme(&self) -> Theme {
         Theme::Dark
     }
+}
+
+async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, io::ErrorKind> {
+    tokio::fs::read_to_string(path)
+        .await
+        .map(Arc::new)
+        .map_err(|error| error.kind())
 }
